d8685ae23265639ba7688fa8e79b2b23c32fb7d3
Ajay Mondal <121559193+mrakmondal6612@users.noreply.github.com>
2024-11-08 07:12:07 +0000

    navbar changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,15 @@ import Header from "./components/Header"
 import Home from "./pages/Home"
 import { useState } from "react";
 import LoginPopup from "./components/LoginPopup";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import Management from "./pages/Management";
+import ClinicalCourses from "./pages/ClinicalCourses";
+import Technology from "./pages/Technology";
+import HealthCarePractice from "./pages/HealthCarePractice";
+import Partner from "./pages/Partner";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 export default function App() {
 	const [showLogin, setShowLogin] = useState(false)
@@ -12,6 +21,15 @@ export default function App() {
 			<Header setShowLogin = {setShowLogin} />
 			<Routes>
 				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/coures" element={<Courses />} />
+				<Route path="/clinical-courses" element={<ClinicalCourses />} />
+				<Route path="/technology" element={<Technology />} />
+				<Route path="/management" element={<Management />} />
+				<Route path="/healthcare-practice-excellence" element={<HealthCarePractice />} />
+				<Route path="/partner" element={<Partner />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="/contact" element={<Contact />} />
 			</Routes>
 		</BrowserRouter>
 	)
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,41 +1,129 @@
-import { useState } from 'react'
-import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from 'react-icons/md'
+import { useState } from "react";
+import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = ({ containerStyles }) => {
-	const [isActive, setisActive] = useState("home")
-	const [showPrograms, setShowPrograms] = useState(false)
+  const [isActive, setisActive] = useState();
+  const [showPrograms, setShowPrograms] = useState(false);
 
-	return (
-		<nav className={`${containerStyles}`}>
-			<a href={'#'} onClick={() => setisActive("home")} className={isActive === "home" ? "active-link" : ""}><div className='flexCenter gap-x-1'><MdHomeFilled />Home</div></a>
-			<a href={'#about'} onClick={() => setisActive("about")} className={isActive === "about" ? "active-link" : ""}><div className='flexCenter gap-x-1'><MdCategory />About</div></a>
-			<div 
-				className={`relative ${isActive === "courses" ? "active-link" : ""}`}
-				onMouseEnter={() => setShowPrograms(true)}
-				onMouseLeave={() => setShowPrograms(false)}
-			>
-				<a 
-				href={'#courses'} 
-				onClick={() => setisActive("courses")}
-				className='flexCenter gap-x-1 cursor-pointer'
-				>
-					<MdShop2 />Programs<IoIosArrowDown />
-				</a>
-				{showPrograms && (
-				<div className="absolute top-full left-0 mt-2 bg-white shadow-lg rounded-md p-2 z-10">
-					<a href={'#clinical-courses'} className="block p-2" onClick={() => setisActive("clinical-courses")}>Clinical Courses</a>
-					<a href={'#technology'} className="block p-2" onClick={() => setisActive("technology")}>Technology</a>
-					<a href={'#management'} className="block p-2" onClick={() => setisActive("management")}>Management</a>
-					<a href={'#healthcare-practice-excellence'} className="block p-2" onClick={() => setisActive("healthcare-practice-excellence")}>Healthcare Practice Excellence</a>
-				</div>
-				)}
-			</div>
-			<a href={'#partners'} onClick={() => setisActive("partners")} className={isActive === "partners" ? "active-link" : ""}><div className='flexCenter gap-x-1'><MdContacts />Partners</div></a>
-			<a href={'#blog'} onClick={() => setisActive("blog")} className={isActive === "blog" ? "active-link" : ""}><div className='flexCenter gap-x-1'><MdContacts />Blog</div></a>
-			<a href={'#contact'} onClick={() => setisActive("contact")} className={isActive === "contact" ? "active-link" : ""}><div className='flexCenter gap-x-1'><MdContacts />Contact</div></a>
-		</nav>
-	)
-}
+  console.log(isActive);
+  return (
+    <nav className={`${containerStyles}`}>
+      <a
+        href={"/"}
+        onClick={() => setisActive("home")}
+        className={isActive === "home" ? "active-link" : ""}
+      >
+        <div className="flexCenter gap-x-1">
+          <MdHomeFilled />
+          Home
+        </div>
+      </a>
+      <a
+        href={"/about"}
+        onClick={() => setisActive("about")}
+        className={isActive === "about" ? "active-link" : ""}
+      >
+        <div className="flexCenter gap-x-1">
+          <MdCategory />
+          About
+        </div>
+      </a>
+      <div
+        className={`relative`}
+        onMouseEnter={() => setShowPrograms(true)}
+        onMouseLeave={() => setShowPrograms(false)}
+      >
+        <a
+          href={"/courses"}
+          onClick={() => setisActive("courses")}
+          className="flexCenter gap-x-1 cursor-pointer"
+        >
+          <div
+            className={`flexCenter gap-x-1 ${
+              isActive === "courses" ? "active-link" : ""
+            }`}
+          >
+            <MdShop2 />
+            Programs
+            <IoIosArrowDown />
+          </div>
+        </a>
+        {showPrograms && (
+          <div className="absolute top-full left-0 bg-white shadow-lg rounded-md p-2 z-10">
+            <a
+              href={"/clinical-courses"}
+              className={`block p-2 ${
+                isActive === "clinical-courses" ? "active-link" : ""
+              }`}
+              onClick={() => setisActive("clinical-courses")}
+            >
+              Clinical Courses
+            </a>
+            <a
+              href={"/technology"}
+              className={`block p-2 ${
+                isActive === "technology" ? "active-link" : ""
+              }`}
+              onClick={() => setisActive("technology")}
+            >
+              Technology
+            </a>
+            <a
+              href={"/management"}
+              className={`block p-2 ${
+                isActive === "management" ? "active-link" : ""
+              }`}
+              onClick={() => setisActive("management")}
+            >
+              Management
+            </a>
+            <a
+              href={"/healthcare-practice-excellence"}
+              className={`block p-2 ${
+                isActive === "healthcare-practice-excellence"
+                  ? "active-link"
+                  : ""
+              }`}
+              onClick={() => setisActive("healthcare-practice-excellence")}
+            >
+              Healthcare Practice Excellence
+            </a>
+          </div>
+        )}
+      </div>
+      <a
+        href={"/partners"}
+        onClick={() => setisActive("partners")}
+        className={isActive === "partners" ? "active-link" : ""}
+      >
+        <div className="flexCenter gap-x-1">
+          <MdContacts />
+          Partners
+        </div>
+      </a>
+      <a
+        href={"/blog"}
+        onClick={() => setisActive("blog")}
+        className={isActive === "blog" ? "active-link" : ""}
+      >
+        <div className="flexCenter gap-x-1">
+          <MdContacts />
+          Blog
+        </div>
+      </a>
+      <a
+        href={"/contact"}
+        onClick={() => setisActive("contact")}
+        className={isActive === "contact" ? "active-link" : ""}
+      >
+        <div className="flexCenter gap-x-1">
+          <MdContacts />
+          Contact
+        </div>
+      </a>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
